57415b958f511c537432ddaa6c6cbf3f1be179bd
svjcan <svj.canada@gmail.com>
2024-12-17 15:36:05 -0500

    Add files via upload

---
+++ c/src/test/java/com/scc/c/CApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scc.c;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
